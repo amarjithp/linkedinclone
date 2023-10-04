e7752d0fc4f7854478e4597911946b49b02fe7bf
Amarjith <72960252+amarjithp@users.noreply.github.com>
2023-10-04 22:12:32 +0530

    done header__left

---
+++ src/HeaderOption.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './HeaderOption.css'
+
+function HeaderOption() {
+  return (
+    <div className="headerOption">HeaderOption</div>
+  );
+}
+
+export default HeaderOption
